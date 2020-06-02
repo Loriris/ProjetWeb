07fe17e84869de1067c27be0ee3016c2c27c6586
Alexis Rafé <sixela815@hotmail.fr>
2020-06-02 15:56:57 +0200

    travail sur la page contact

---
+++ js/nous_contacter.js
@@ -0,0 +1,30 @@
+/* 
+    Description : JS pour la page nous_contacter
+    Created on : 30 avr. 2020, 10:23:27
+    Auteur : Loris Martinez - Alexis Rafé - Nicolas Lebon
+    Email : lorismartinez1202@gmail.com - alexisrafe@gmail.com - niko83136@gmail.com
+*/
+
+/*La fonction principale de ce script est d'empêcher l'envoi du formulaire si un champ a été mal rempli
+*et d'appliquer les styles de validation aux différents éléments de formulaire*/
+(function()
+{
+    'use strict';
+    window.addEventListener('load', function() 
+    {
+        let forms = document.querySelectorAll('.needs-validation');
+        Array.prototype.filter.call(forms, function(form) 
+        {
+            form.addEventListener('submit', function(event) 
+            {
+                if (form.checkValidity() === false) 
+                    {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
