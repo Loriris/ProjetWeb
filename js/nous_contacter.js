b8ff2ecbc50bd44a995520c006e0440ff77f8a1b
Alexis Rafé <sixela815@hotmail.fr>
2020-06-04 13:36:51 +0200

    fix des classes dans les articles

--- js/nous_contacter.js
+++ js/nous_contacter.js
@@ -5,26 +5,22 @@
     Email : lorismartinez1202@gmail.com - alexisrafe@gmail.com - niko83136@gmail.com
 */
 
-/*La fonction principale de ce script est d'empêcher l'envoi du formulaire si un champ a été mal rempli
-*et d'appliquer les styles de validation aux différents éléments de formulaire*/
-(function()
+//La fonction principale de ce script est d'empêcher l'envoi du formulaire si un champ a été mal rempli
+//et d'appliquer les styles de validation aux différents éléments de formulaire
+window.addEventListener('load', function ()
 {
-    'use strict';
-    window.addEventListener('load', function() 
+    let forms = document.querySelectorAll('.needs-validation');
+    Array.prototype.filter.call(forms, function (form)
     {
-        let forms = document.querySelectorAll('.needs-validation');
-        Array.prototype.filter.call(forms, function(form) 
+        form.addEventListener('submit', function (event)
         {
-            form.addEventListener('submit', function(event) 
+            if (form.checkValidity() === false)
             {
-                if (form.checkValidity() === false) 
-                    {
-                        event.preventDefault();
-                        event.stopPropagation();
-                    }
-                    form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}, false);
 
