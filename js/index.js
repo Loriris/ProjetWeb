c09e9b4cdb6e7c3f4f43a7ee2f75e8a13b0c57cd
Alexis Rafé <sixela815@hotmail.fr>
2020-06-05 11:17:04 +0200

    ajout de bouton pour faciliter la saisie dans le recherche

--- js/index.js
+++ js/index.js
@@ -8,10 +8,35 @@
 //Fonction de recherche d'article
 window.addEventListener('load', function ()
 {
+    let body = document.querySelectorAll('.body');
+    let dropDownMenu = document.querySelectorAll('.dropdown-menu');
+    let btnSujet = document.querySelectorAll('.btn_sujet');
+    let input = document.querySelectorAll('#texteARechercher');
+    //ajout de bouton pour faciliter la saisie de sujet
+    for(let index = 0; index< btnSujet.length; index++)
+    {
+        btnSujet[index].addEventListener('click', function()
+        {
+            input[0].value = btnSujet[index].textContent;
+            dropDownMenu[0].classList.add("block");
+            dropDownMenu[1].classList.add("block");
+            
+        });
+    }
+    
+    body[0].addEventListener('click',function() 
+    {
+            dropDownMenu[0].classList.add("none");
+            dropDownMenu[1].classList.add("none");
+            dropDownMenu[0].classList.remove("block");
+            dropDownMenu[1].classList.remove("none");
+    });
+    
     let btnRecherche = document.querySelectorAll('#recherche');
     btnRecherche[0].addEventListener('click', function ()
     {
-        let input = document.querySelectorAll('#texteARechercher');
+        dropDownMenu[0].classList.remove("block");
+        dropDownMenu[1].classList.remove("block");
         let text = input[0].value.toLowerCase();
         
         // récupération de tout les article de la page
