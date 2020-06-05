0916e1a11c4a30190d8e82a774baa64104282c0b
Alexis Rafé <sixela815@hotmail.fr>
2020-06-05 11:55:30 +0200

    réorgansisation des scripts

--- js/recherche.js
+++ js/recherche.js
@@ -1,9 +1,10 @@
 /* 
-    Description : JS pour la page index
+    Description : JS pour la recherche de sujet
     Created on : 30 avr. 2020, 10:23:27
     Auteur : Loris Martinez - Alexis Rafé - Nicolas Lebon
     Email : lorismartinez1202@gmail.com - alexisrafe@gmail.com - niko83136@gmail.com
 */
+
 
 //Fonction de recherche d'article
 window.addEventListener('load', function ()
@@ -72,38 +73,6 @@ window.addEventListener('load', function ()
             {
                 article[index].classList.add("block");
             }
-        }
-    });
-});
-
-//Fonction gérant la fenêtre de connexion 
-window.addEventListener('load', function()
-{
-    let frame = document.querySelectorAll("#connexion");
-    let btn = document.querySelectorAll(".connexion-btn");
-    let close = document.querySelectorAll(".close");
-    
-    //si clique sur le bouton on affiche la frame
-    btn[0].addEventListener('click',function() 
-    {
-        frame[0].classList.add("block");
-        frame[0].classList.remove("none");
-    });
-    
-    //si on appuie sur la croix on ferme la frame
-    close[0].addEventListener('click',function() 
-    {
-        frame[0].classList.add("none");
-        frame[0].classList.remove("block");
-    });
-    
-    //si on n'importe ou en dehors de la frame on la ferme
-    window.addEventListener('click',function(event) 
-    {
-        if(event.target === frame[0])
-        {
-          frame[0].classList.add("none");
-          frame[0].classList.remove("block");
         }
     });
 });
