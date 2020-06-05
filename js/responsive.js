8ec2a37f8395d7974f2b96772e367dddea9aa40e
Alexis Rafé <sixela815@hotmail.fr>
2020-06-05 15:24:27 +0200

    ajout règle 400px au script responsive + début article rugby

--- js/responsive.js
+++ js/responsive.js
@@ -9,18 +9,26 @@
 let responsive = function () {
     
     let dropDownResponsive = document.querySelectorAll('.btn-group');
+    
+    //Si l'écran est plus petit que 992px le menu se déroule a gauche
     if (window.matchMedia("(max-width : 992px)").matches)
     {
         dropDownResponsive[0].classList.add('dropleft');
     }
-    else if(window.matchMedia("(min-width : 992px)").matches)
+    
+    //si plus grand a droite
+    if(window.matchMedia("(min-width : 992px)").matches)
     {
-        if(dropDownResponsive[0].classList.contains('dropleft'))
-        {
             dropDownResponsive[0].classList.remove('dropleft');
-        }
+    }
+    
+    //si plus petit que 400px a droite
+    if (window.matchMedia("(max-width : 400px)").matches)
+    {
+        dropDownResponsive[0].classList.remove('dropleft');
     }
 };
 
+//On ajout les event déclenchant la focntion
 window.addEventListener('load', responsive);
 window.addEventListener('resize', responsive);
